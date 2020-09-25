c5b67da6fa00df7a6ed0f5034b5708bd4ea3b4ba
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-09-25 23:13:36 +1200

    Add more complex animation

--- packages/ui/src/component/animation/animation.component.tsx
+++ packages/ui/src/component/animation/animation.component.tsx
@@ -29,30 +29,140 @@ export const MatterStepThree = () => {
     const Render = Matter.Render
     const World = Matter.World
     const Bodies = Matter.Bodies
+    const Runner = Matter.Runner
+    const Composite = Matter.Composite
+    const Composites = Matter.Composites
+    const Common = Matter.Common
+    const MouseConstraint = Matter.MouseConstraint
+    const Mouse = Matter.Mouse
 
     const engine = Engine.create({})
-
-    const render = Render.create({
-      element: boxRef.current,
-      engine: engine,
-      canvas: canvasRef.current,
-      options: {
-        background: "transparent",
+    const world = engine.world
+// create renderer
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 500,
         wireframes: false,
-      },
+        background: '#ffffff'
+    }
+});
+
+Render.run(render);
+
+// create runner
+const runner = Runner.create();
+Runner.run(runner, engine);
+
+// define our categories (as bit fields, there are up to 32 available)
+const defaultCategory = 0x0001,
+    redCategory = 0x0002,
+    greenCategory = 0x0004,
+    blueCategory = 0x0008;
+
+const redColor = '#C44D58',
+    blueColor = '#4ECDC4',
+    greenColor = '#C7F464';
+
+// add floor
+World.add(world, Bodies.rectangle(400, 600, 900, 50, { 
+    isStatic: true,
+    render: {
+        fillStyle: 'transparent',
+        lineWidth: 1
+    } 
+}));
+
+// create a stack with varying body categories (but these bodies can all collide with each other)
+World.add(world,
+    Composites.stack(275, 100, 5, 9, 10, 10, function(x, y, column, row) {
+        let category = redCategory,
+            color = redColor;
+
+        if (row > 5) {
+            category = blueCategory;
+            color = blueColor;
+        } else if (row > 2) {
+            category = greenCategory;
+            color = greenColor;
+        }
+
+        return Bodies.circle(x, y, 20, {
+            collisionFilter: {
+                category: category
+            },
+            render: {
+                strokeStyle: color,
+                opacity: 0.4,
+                lineWidth: 1
+            }
+        });
     })
+);
 
-    const floor = Bodies.rectangle(0, 0, 0, STATIC_DENSITY, {
-      isStatic: true,
-      render: {
-        fillStyle: "blue",
-      },
+// this body will only collide with the walls and the green bodies
+World.add(world,
+    Bodies.circle(310, 40, 30, {
+        collisionFilter: {
+            mask: defaultCategory | greenCategory
+        },
+        render: {
+            fillStyle: greenColor
+        }
     })
+);
 
-    World.add(engine.world, [floor])
+// this body will only collide with the walls and the red bodies
+World.add(world,
+    Bodies.circle(400, 40, 30, {
+        collisionFilter: {
+            mask: defaultCategory | redCategory
+        },
+        render: {
+            fillStyle: redColor
+        }
+    })
+);
 
-    Engine.run(engine)
-    Render.run(render)
+// this body will only collide with the walls and the blue bodies
+World.add(world,
+    Bodies.circle(480, 40, 30, {
+        collisionFilter: {
+            mask: defaultCategory | blueCategory
+        },
+        render: {
+            fillStyle: blueColor
+        }
+    })
+);
+
+// add mouse control
+const mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// red category objects should not be draggable with the mouse
+mouseConstraint.collisionFilter.mask = defaultCategory | blueCategory | greenCategory;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 800, y: 500 }
+});
 
     setContraints(boxRef.current.getBoundingClientRect())
     setScene(render)
@@ -95,44 +205,8 @@ export const MatterStepThree = () => {
     }
   }, [scene, constraints])
 
-  /*eslint-disable */
-  useEffect(() => {
-    // Add a new "ball" everytime `someStateValue` changes
-    if (scene) {
-      let { width } = constraints
-      let randomX = Math.floor(Math.random() * -width) + width
-      Matter.World.add(
-        scene.engine.world,
-        Matter.Bodies.circle(randomX, -PARTICLE_SIZE, PARTICLE_SIZE, {
-          restitution: PARTICLE_BOUNCYNESS,
-        })
-      )
-    }
-  }, [scene, someStateValue])
-  /*eslint-enable */
 
   return (
-    <div
-      style={{
-        position: "relative",
-        border: "1px solid white",
-        padding: "8px",
-        height: "300px"
-      }}
-    >
-      <button
-        style={{
-          cursor: "pointer",
-          display: "block",
-          textAlign: "center",
-          marginBottom: "16px",
-          width: "100%",
-        }}
-        onClick={() => handleClick()}
-      >
-        Click me
-      </button>
-
       <div
         ref={boxRef}
         style={{
@@ -146,6 +220,5 @@ export const MatterStepThree = () => {
       >
         <canvas ref={canvasRef} />
       </div>
-    </div>
   )
 }
