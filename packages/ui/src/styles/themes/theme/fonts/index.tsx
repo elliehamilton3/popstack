a1e4450a303ea01adeb55c531ec78530d152cef3
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-09-28 19:23:32 +1300

    Tidy up fonts

--- packages/ui/src/styles/themes/theme/fonts/index.tsx
+++ packages/ui/src/styles/themes/theme/fonts/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Circular } from './circular';
-import { CircularBold } from './circularBold';
 
 export const FontLoader: React.FC = () => (
   <>
-    <Circular />
-    <CircularBold />
   </>
 );
 
