5b5cd32f2a218deac42d243f8df9829511dd1cfe
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-10-03 17:20:21 +1300

    Tidy up

--- packages/ui/src/component/animation/animation1.stories.tsx
+++
@@ -1,6 +0,0 @@
-// /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// import React from 'react';
-// import {MatterStepThree} from './animation1.component';
-
-// export default { title: 'MatterStepThree 1', component: MatterStepThree };
-// export const Basic = () => <MatterStepThree></MatterStepThree>;
