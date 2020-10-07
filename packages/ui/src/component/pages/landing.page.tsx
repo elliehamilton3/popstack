246d4d885346de62a6da0bdeeb0c5908c75e2dcb
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-10-07 15:04:07 +1300

    Fix resizing width

--- packages/ui/src/component/pages/landing.page.tsx
+++ packages/ui/src/component/pages/landing.page.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
   },
   shapes:{
     position: "absolute",
-    top: "calc(448px - 35%)",
+    top: "calc(488px - 26vw)",
     left: 0,
     width: "100%",
     height: "100%",
