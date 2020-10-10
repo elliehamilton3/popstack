fa16a2637eafc3251a2d87893fd3720f7351c891
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-10-10 20:32:19 +1300

    Add little animation to logo

--- packages/ui/src/component/navbar/navbar.component.tsx
+++ packages/ui/src/component/navbar/navbar.component.tsx
@@ -1,8 +1,51 @@
 import * as React from "react";
-import { AppBar, Toolbar, IconButton, Grid } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Grid, makeStyles } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
+import { NavLink } from "react-router-dom";
+const useStyles = makeStyles(() => ({
+  link: {
+    textDecoration: "none",
+  },
+  logo: {
+    "& rect": {
+      transition: "transform 400ms ease"
+    },
+    "& circle:nth-of-type(1)": {
+      transition: "transform 400ms ease"
+    },
+    "& circle:nth-of-type(2)": {
+      transition: "transform 400ms ease"
+    },
+    "& path": {
+      transition: "transform 400ms ease"
+    },
+    "&:hover": {
+      "& rect": {
+
+        transform: "translate(80px, -100px)",
+        transition: "transform 400ms ease"
+      },
+      "& circle:nth-of-type(1)": {
+
+        transform: "translate(-55px, 100px)",
+        transition: "transform 400ms ease"
+      },
+      "& circle:nth-of-type(2)": {
+
+        transform: "translate(100px, 0px)",
+        transition: "transform 400ms ease"
+      },
+      "& path": {
+        transform: "rotate(-15deg) translate(-30px, 20px)",
+        transition: "transform 400ms ease"
+      }
+    }
+  }
+
+}));
 
 const Navbar: React.FunctionComponent = () => {
+  const classes = useStyles();
   return (
     <AppBar position="fixed">
       <Toolbar>
@@ -20,14 +63,20 @@ const Navbar: React.FunctionComponent = () => {
               alignContent="center"
               spacing={1}
             >
+            
               <Grid item>
+              <NavLink
+                to="/"
+                exact
+              >
                 <Grid container spacing={2}>
                   <Grid item>
                     <svg
                       height="32"
-                      viewBox="0 0 390 312"
+                      viewBox="-100 -50 600 360"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
+                      className={classes.logo}
                     >
                       <rect
                         x="148"
@@ -36,6 +85,7 @@ const Navbar: React.FunctionComponent = () => {
                         height="148"
                         transform="rotate(-31.2927 148 76.8728)"
                         fill="#62CFF1"
+      
                       />
                       <circle cx="70" cy="108" r="70" fill="#FFD84C" />
                       <circle cx="320" cy="242" r="70" fill="#FFB7D5" />
@@ -87,7 +137,9 @@ const Navbar: React.FunctionComponent = () => {
                       />
                     </svg>
                   </Grid>
+
                 </Grid>
+                </NavLink>
               </Grid>
             </Grid>
           </Grid>
